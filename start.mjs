152856ffaccaa64e0a9ff9e04017864ca2ddd756
Mirko-linux <donatomirko64@gmail.com>
2025-06-28 18:37:53 +0200

    Update start.mjs

--- start.mjs
+++ start.mjs
@@ -1,5 +1,7 @@
-import { createServer } from 'puter'
+import puterPkg from 'puter'
 import config from './puter.config.mjs'
 
+const createServer = puterPkg.createServer || puterPkg.default || puterPkg
 config.port = process.env.PORT || 1337
+
 createServer(config)
